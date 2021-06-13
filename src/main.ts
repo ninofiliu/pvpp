331af074eaa561a2328bd4a20e20f0ddb204ef06
Nino Filiu <nino.filiu@gmail.com>
2021-06-13 18:23:21 +0200

    add ins and outs

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import createSpiral from './createSpiral';
 
 // ________________0_______1_______2_______3_______4_______5_______6_______7_____
 // ________________..._____..1_____.1._____.11_____1.._____1.1_____11._____111___
-const palette = ['#000', '#222', '#333', '#555', '#777', '#999', '#bbb', '#fff'];
-const reveal = [1, 2, 4];
-const batch = 500;
+const palette = ['#f00', '#000', '#222', '#444', '#666', '#999', '#bbb', '#fff'];
+const reveal = [];
+const batch = 2000;
 
 const channels = [0, 1, 2] as const;
 
 const canvas = document.querySelector('canvas');
 const image = new Image();
-image.src = '/in/nelle.jpg';
+image.src = '/in/gaby3.jpg';
 image.onload = () => {
   const { width, height } = image;
   canvas.width = width;
@@ -18,7 +18,7 @@ image.onload = () => {
   const ctx = canvas.getContext('2d');
   ctx.drawImage(image, 0, 0);
   const imageData = ctx.getImageData(0, 0, width, height);
-  ctx.filter = 'grayscale(100%) contrast(1.5)';
+  ctx.filter = 'grayscale(100%) contrast(1)';
   ctx.drawImage(image, 0, 0);
   const greyImageData = ctx.getImageData(0, 0, width, height);
   ctx.filter = 'grayscale(0%) contrast(1)';
@@ -29,8 +29,8 @@ image.onload = () => {
     channel,
     stopAt: 0.8,
     kind: 'compressed',
-    divider: 8,
-    multiplier: 4,
+    divider: 15,
+    multiplier: 8,
     quality: 5,
   }));
 
@@ -46,12 +46,12 @@ image.onload = () => {
         spiral.move();
         const n = (width * spiral.y + spiral.x);
         const drawnIndex = 3 * n;
+        drawn[drawnIndex + channel] = 1;
         const paletteIndex = 4 * drawn[drawnIndex] + 2 * drawn[drawnIndex + 1] + drawn[drawnIndex + 2];
         const paletteColor = palette[paletteIndex];
         const srcColor = `rgb(${greyImageData.data[4 * n]},${greyImageData.data[4 * n + 1]},${greyImageData.data[4 * n + 2]})`;
         ctx.fillStyle = reveal.includes(paletteIndex) ? srcColor : paletteColor;
         ctx.fillRect(spiral.x, spiral.y, 1, 1);
-        drawn[drawnIndex + channel] = 1;
       }
     }
     requestAnimationFrame(loop);
